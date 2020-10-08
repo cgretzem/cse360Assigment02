0df902c2adb5422911563034f4ce74116774937b
Cooper Gretzema <cgretzem@asu.edu>
2020-10-07 17:44:34 -0700

    Added functionality to AddingMachine

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -1,49 +1,55 @@
 package cse360assignment02;
-
 /**
  * A class that is able to get the total, add, and subtract. Includes a toString
  * @author Cooper Gretzema
  */
 public class AddingMachine {
   private int total;
-  
+  private String history;
   /**
    * Constructor in order to initialize an AddingMachine Object
    * @return AddingMachine
   */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0 ";
   }
   /**
    * Method to get the private instance variable total from AddingMachine Object
    * @return total  The private instance variable total
   */
   public int getTotal () {
-    return 0;
+    return total;
   }
   /**
    * Method to add a new number to the total stored in the AddingMachine
    * @param value  the value that is to be added to the total
   */
   public void add (int value) {
+	  total += value;
+	  history += "+ " + value + " ";
   }
   /**
    * Method to subtract a new number to the total stored in the AddingMachine
    * @param value  the value that is to be subtracted to the total
   */
   public void subtract (int value) {
+	  total -= value;
+	  history += "- " + value + " ";
   }
   /**
-   * Method to print a history of all operations and the final total
-   * @return history  a history of all operations and the current total
+   * Method to print a history of all operations 
+   * @return history  a history of all operations
   */
   public String toString () {
-    return "";
+    return history;
   }
   /**
    * Method to clear all the operations on the calculator and set total to 0
    * @return void
   */
   public void clear() {
+	  total = 0;
+	  history = "0 ";
   }
 }
